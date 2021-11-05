3b90a28f79abb8eaf954ac57ab8a517074326b5e
hackingthesystems <91442571+hackingthesystems@users.noreply.github.com>
2021-11-05 17:07:10 -0400

    Update betternotebad.js

--- betternotebad.js
+++ betternotebad.js
@@ -1,34 +1,39 @@
+(function () {
+    const script = document.createElement("script");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+    script.type = 'text/javascript';
+    script.addEventListener('load', () => {
+        console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
+    });
+    document.head.appendChild(script);
+})();
+
 let frame = document.createElement("IFRAME");
 
 frame.style.visibility = "visible";
 frame.style.width = "25%";
 frame.style.border = "3px solid black";
-frame.style.z-index = "6942069420";
+frame.style.zIndex = "6942069420";
 frame.style.position = "fixed";
 frame.style.bottom = "0";
 frame.style.right = "0";
 frame.style.height = "50%";
 frame.style.background = "#FFFFFF";
 
-frame.id = "notepadbutbetter";
-frame.value = "ON";
+frame.id = "ON";
 frame.src = "data:text/html, <html contenteditable>";
 
 document.body.appendChild(frame);
 
-
-addEventListener("keypress", key, false);
-
-var button = document.getElementById("notepadbutbetter");
-
-function key(event) {
-	if (event.keyCode === 192) {
-		if (button.value == "OFF") {
-			button.value = "ON";
-			button.setAttribute("style", "visibility: visible;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;");
-		}
-	} else if (button.value == "ON") {
-		button.value = "OFF";
-		button.setAttribute("style", "visibility: hidden;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;");
-	}
-}
+$(document).keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '11'){
+        if (frame.id == "OFF") {
+            frame.id = "ON";
+            frame.setAttribute("style", "visibility: visible; width: 25%; border: 3px solid black; zIndex: 6942069420; position: fixed; bottom: 0; right: 0; height: 50%; background: #FFFFFF");
+        } else if (frame.id == "ON") {
+            frame.id = "OFF";
+            frame.setAttribute("style", "visibility: hidden; width: 25%; border: 3px solid black; zIndex: 6942069420; position: fixed; bottom: 0; right: 0; height: 50%; background: #FFFFFF");
+        }
+    }
+});
