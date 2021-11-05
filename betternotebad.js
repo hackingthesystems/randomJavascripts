02cc90b6b3543ca115230ad02b4e4bed42812711
hackingthesystems <91442571+hackingthesystems@users.noreply.github.com>
2021-11-05 09:10:49 -0400

    Update betternotebad.js

--- betternotebad.js
+++ betternotebad.js
@@ -1,18 +1,18 @@
-document.body.innerHTML += '<iframe style=\"visibility: visible;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;\" src=\"data:text/html, <html contenteditable>" id=\"notepadbutbetter" value="ON">';
 
+let frame = document.createElement("IFRAME");
 
-addEventListener("keypress", key, false);
+frame.style.visibility = "visible";
+frame.style.width = "25%";
+frame.style.border = "3px solid black";
+frame.style.z-index = "6942069420";
+frame.style.position = "fixed";
+frame.style.bottom = "0";
+frame.style.right = "0";
+frame.style.height = "50%";
+frame.style.background = "#FFFFFF";
 
-var button = document.getElementById("notepadbutbetter")
+frame.id = "notepadbutbetter";
+frame.value = "ON";
+frame.src = "data:text/html, <html contenteditable>";
 
-function key(event) {
-	if (event.keyCode === 192) {
-		if (button.value == "OFF") {
-			button.value = "ON";
-			button.setAttribute("style", "visibility: visible;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;")
-		}
-	} else if (button.value == "ON") {
-		button.value = "OFF";
-		button.setAttribute("style", "visibility: hidden;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;")
-	}
-}
+document.body.appendChild(frame);
