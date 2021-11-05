63faa57f88a961d05cc338db8c5246e43a61f868
hackingthesystems <91442571+hackingthesystems@users.noreply.github.com>
2021-11-05 09:12:07 -0400

    Update betternotebad.js

--- betternotebad.js
+++ betternotebad.js
@@ -1,4 +1,3 @@
-
 let frame = document.createElement("IFRAME");
 
 frame.style.visibility = "visible";
@@ -16,3 +15,20 @@ frame.value = "ON";
 frame.src = "data:text/html, <html contenteditable>";
 
 document.body.appendChild(frame);
+
+
+addEventListener("keypress", key, false);
+
+var button = document.getElementById("notepadbutbetter");
+
+function key(event) {
+	if (event.keyCode === 192) {
+		if (button.value == "OFF") {
+			button.value = "ON";
+			button.setAttribute("style", "visibility: visible;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;");
+		}
+	} else if (button.value == "ON") {
+		button.value = "OFF";
+		button.setAttribute("style", "visibility: hidden;width: 25%;border: 3px solid black; z-index: 100000000000; position: fixed; bottom: 0; right: 0; height: 50%;background: #FFFFFF;");
+	}
+}
