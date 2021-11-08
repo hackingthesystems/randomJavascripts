44e3d92609b4e90507583b5bac7ffec36f90c135
hackingthesystems <91442571+hackingthesystems@users.noreply.github.com>
2021-11-08 09:14:40 -0500

    Update betternotebad.js

--- betternotebad.js
+++ betternotebad.js
@@ -16,6 +16,7 @@ var open = false;
 frame.style.visibility = "visible";
 frame.style.width = "25%";
 frame.style.border = "3px solid black";
+frame.style.zIndex = "100000000000000000";
 frame.style.position = "fixed";
 frame.style.bottom = "0";
 frame.style.right = "0";
@@ -31,12 +32,12 @@ document.body.appendChild(frame);
 
 //FUNCTIONS
 function hide(){
-    frame.setAttribute("style", "visibility: hidden; width: 25%; border: 3px solid black; position: fixed; bottom: 0; right: 0; height: 50%; background: #FFFFFF");
+    frame.setAttribute("style", "visibility: hidden; width: 25%; border: 3px solid black; zIndex: "100000000000000000"; position: fixed; bottom: 0; right: 0; height: 50%; background: #FFFFFF");
     open = false;
 }
 
 function show(){
-    frame.setAttribute("style", "visibility: visible; width: 25%; border: 3px solid black; position: fixed; bottom: 0; right: 0; height: 50%; background: #FFFFFF");
+    frame.setAttribute("style", "visibility: visible; width: 25%; border: 3px solid black; zIndex: "100000000000000000"; position: fixed; bottom: 0; right: 0; height: 50%; background: #FFFFFF");
     open = true;
 }
 
@@ -44,7 +45,6 @@ function toggle() {
     open ? hide() : show();
 }
 
-//ON-KEY-CLICK
 window.addEventListener("keydown", checkKeyPressed, false);
 
 function checkKeyPressed(evt) {
